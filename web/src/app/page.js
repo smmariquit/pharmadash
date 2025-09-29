1ab73f7b038e2bf89edd49a79571c83037da5123
Waffen Sultan <waffen.sultan@gmail.com>
2025-09-29 22:04:14 +0800

    chore: delete unnecessary files

--- web/src/app/page.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = RootPage;
-const jsx_runtime_1 = require("react/jsx-runtime");
-function RootPage() {
-    return ((0, jsx_runtime_1.jsx)("div", { children: (0, jsx_runtime_1.jsx)("div", { children: "Test" }) }));
-}
-//# sourceMappingURL=page.js.map
