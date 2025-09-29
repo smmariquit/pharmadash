b2acbef9676db40571733fad2d852fd836f505ff
Waffen Sultan <waffen.sultan@gmail.com>
2025-09-29 22:05:25 +0800

    chore: delete unnecessary files

--- web/next.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const nextConfig = {
-    output: "export",
-};
-exports.default = nextConfig;
-//# sourceMappingURL=next.config.js.map
