0b43c6f95ed005d4f461494ec65a23b98abf8694
Waffen Sultan <waffen.sultan@gmail.com>
2025-09-29 20:58:18 +0800

    chore: add boilerplates

---
+++ web/src/app/api/add-record/route.ts
@@ -0,0 +1,7 @@
+// This is Route Handler in Next.js
+// Basically a backend API route
+
+// This is an API endpoint that accepts a GET request
+export default function GET(request: Request) {
+  // Backend logic here...
+}
