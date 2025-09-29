adf56494f7b8d846a3ade0f64b270b0c1023c005
semariquit@gmail.com <semariquit@gmail.com>
2025-09-29 20:29:21 +0800

    chore: sync with gitea

---
+++ web/src/app/page.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = RootPage;
+const jsx_runtime_1 = require("react/jsx-runtime");
+function RootPage() {
+    return ((0, jsx_runtime_1.jsx)("div", { children: (0, jsx_runtime_1.jsx)("div", { children: "Test" }) }));
+}
+//# sourceMappingURL=page.js.map
